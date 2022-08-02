b45e1247bfc59f01e5aea9516cd6a3bc2a14f830
Ramiro <109646337+ramiro-menendez@users.noreply.github.com>
2022-08-02 02:42:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type", {
+    strings: ["ingeniero (est.).", "desarrollador.", "diseñador UX/UI.", "editor."],
+    typeSpeed: 35,
+    backSpeed: 25,
+    backDelay: 750,
+    loop: true,
+})
