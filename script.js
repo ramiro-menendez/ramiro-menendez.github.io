5a2b196ffe14879bfe62b8d5c9f680c4a7075d85
Ramiro <109646337+ramiro-menendez@users.noreply.github.com>
2022-09-26 23:55:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-var typed = new Typed(".auto-type", {
-    strings: ["ingeniero (est.).", "desarrollador.", "diseñador UX/UI.", "editor."],
-    typeSpeed: 35,
-    backSpeed: 25,
-    backDelay: 750,
-    loop: true,
-})
+
+/*
+Esto sube el titulo 70px si se hace scroll hacia abajo
+Lo restaura a su posicion original si se hace hacia arriba.
+*/
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("titulo").style.top = "0";
+  } 
+  else {
+    document.getElementById("titulo").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+} 
