00205f81f87b9ab7226003b41c4896c9c8393853
Ramiro <109646337+ramiro-menendez@users.noreply.github.com>
2022-09-27 19:33:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -21,8 +21,14 @@ function subirMenu()
 {
     if (document.getElementsByClassName("menuAgregarItem")[0].style.marginTop=="0px") {
       document.getElementsByClassName("menuAgregarItem")[0].style.marginTop="-300px";
+      document.getElementsByClassName("botonMenu")[0].style.transform = "rotate(180deg)";
+      document.getElementsByClassName("contenido")[0].style.opacity = "1";
+      document.getElementsByClassName("agregarCarrito")[0].style.opacity = "1";
     }
     else {
       document.getElementsByClassName("menuAgregarItem")[0].style.marginTop="0px";
+      document.getElementsByClassName("botonMenu")[0].style.transform = 'rotate(0deg)';
+      document.getElementsByClassName("contenido")[0].style.opacity = "0";
+      document.getElementsByClassName("agregarCarrito")[0].style.opacity = "0";
     }
 }
