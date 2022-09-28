7535458f8f1290ea865579f9cc0243b9bd47eb9a
Ramiro <109646337+ramiro-menendez@users.noreply.github.com>
2022-09-28 16:57:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -27,16 +27,16 @@ en opacidad 0.
 function subirMenu()
 {
     if (document.getElementsByClassName("menuAgregarItem")[0].style.marginTop=="0px") {
-      document.getElementsByClassName("menuAgregarItem")[0].style.marginTop="-300px";
+      document.getElementsByClassName("menuAgregarItem")[0].style.marginTop="-400px";
       document.getElementsByClassName("botonMenu")[0].style.transform = "rotate(180deg)";
-      for (i=0; i<=3; i++) {
+      for (i=0; i<=4; i++) {
         document.getElementsByClassName("aparecer")[i].style.opacity = "1";
       }
     }
     else {
       document.getElementsByClassName("menuAgregarItem")[0].style.marginTop="0px";
       document.getElementsByClassName("botonMenu")[0].style.transform = 'rotate(0deg)';
-      for (i=0; i<=3; i++) {
+      for (i=0; i<=4; i++) {
         document.getElementsByClassName("aparecer")[i].style.opacity = "0";
       }
     }
